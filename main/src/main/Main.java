e084c57169a5a5735019b655dc4a685915012b0c
victor.salves5 <victor.salves5@CAS555495W1021.senacsp.edu.br>
2019-08-28 19:25:38 -0300

    exercicios até a aula 3

--- main/src/main/Main.java
+++ main/src/main/Main.java
@@ -10,7 +10,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // on - sem algoritimos quadraticos, sem loping dentro de loping , um algoritimo linear que chegue em um resultado.
+        
     }
     
 }
